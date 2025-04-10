26c3275aa8cd2f2737833f4ec3d9da40f958b087
pgmtx <programutox@disroot.org>
2025-04-10 11:58:50 +0200

    Add bar charts for questions with multiple choices

--- main.py
+++ main.py
@@ -16,35 +16,44 @@ def percentage_format(x):
     return f"{x:.2f}%"
 
 
+def has_multiple_choices(answers_counts):
+    return any(";" in key for key in answers_counts)
+
+
+def show_bar_chart(answers_counts):
+    singles_answers_counts = dict()
+    for key, value in answers_counts.items():
+        single_answers = key.split(";")
+        for a in single_answers:
+            singles_answers_counts[a] = singles_answers_counts.get(a, 0) + value
+    plt.bar(
+        x=singles_answers_counts.keys(),
+        height=singles_answers_counts.values()
+    )
+
+
 def main():
-    # data_frame = pd.DataFrame(dict(
-    #     r=[1, 5, 2, 2, 3],
-    #     theta=["Early access to documents","Data security","Browsing speed",
-    #        "Mobile compatibility", "Design and ergonomics"]
-    # ))
-    # figure = px.line_polar(data_frame, r="r", theta="theta", line_close=True)
-    # figure.show()
     data_frame = pd.read_csv("assets/data.csv")
     # On enlève la colonne Horodateur
-    # tilde = complémentaire
+    # Le tilde correspond au complémentaire
     all_answers = data_frame.loc[:, ~data_frame.columns.isin(["Horodateur"])]
-
+    #all_answers = data_frame.loc[:]
     plt.title("Résultats")
-    for i, (question, answers) in enumerate(all_answers.items()):
+    for question, answers in all_answers.items():
         answers_counts = filtered_counter(answers)
         # Certaines questions son décomposées en "questions fragmentées",
         # donc on les filtre pour le moment.
         # Ces fragments contiennent des bouts de réponse comme "[Logs]"
-        if "[" in question or any(";" in key for key in answers_counts):
+        if "[" in question:
             continue
-        if i > 3:
-            break
-
-        # index_line, index_columns = divmod(i, columns)
-        # axes[index_line, index_columns].set_title(question)
-        # axes[index_line, index_columns].pie(answers_counts.values(), labels=answers_counts.keys())
+        if has_multiple_choices(answers_counts):
+            show_bar_chart(answers_counts)
+        else:
+            # index_line, index_columns = divmod(i, columns)
+            # axes[index_line, index_columns].set_title(question)
+            # axes[index_line, index_columns].pie(answers_counts.values(), labels=answers_counts.keys())
+            plt.pie(answers_counts.values(), labels=answers_counts.keys(), autopct=percentage_format, shadow=True)
         plt.title(question)
-        plt.pie(answers_counts.values(), labels=answers_counts.keys(), autopct=percentage_format, shadow=True)
         plt.show()
 
 
