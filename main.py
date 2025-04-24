debff7164d2140d3a3a4e03c32371a9e1f28d248
pgmtx <programutox@disroot.org>
2025-04-24 10:22:15 +0200

    Exporté les résultats et crée un script bash

--- main.py
+++ main.py
@@ -37,24 +37,38 @@ def main():
     # On enlève la colonne Horodateur
     # Le tilde correspond au complémentaire
     all_answers = data_frame.loc[:, ~data_frame.columns.isin(["Horodateur"])]
-    #all_answers = data_frame.loc[:]
-    plt.title("Résultats")
-    for question, answers in all_answers.items():
+    open_questions = (
+        "According to you, why?",
+        "Do you take measures to protect your data when you connect to your LMS? If so, which ones?"
+    )
+
+    for i, (question, answers) in enumerate(all_answers.items()):
         answers_counts = filtered_counter(answers)
         # Certaines questions son décomposées en "questions fragmentées",
         # donc on les filtre pour le moment.
         # Ces fragments contiennent des bouts de réponse comme "[Logs]"
         if "[" in question:
             continue
+
+        if "example" in question or question in open_questions:
+            with open(f"output/question{i+1}.txt", 'w') as file:
+                file.write(f"Question : {question}\n\n")
+                file.write('\n'.join('- ' + key for key in answers_counts.keys()))
+            continue
+
+        print(question)
         if has_multiple_choices(answers_counts):
+            plt.figure(figsize=(13, 7))
+            plt.title(question)
+            plt.tight_layout()
             show_bar_chart(answers_counts)
         else:
-            # index_line, index_columns = divmod(i, columns)
-            # axes[index_line, index_columns].set_title(question)
-            # axes[index_line, index_columns].pie(answers_counts.values(), labels=answers_counts.keys())
+            plt.figure(figsize=(12, 7))
+            plt.title(question)
+            plt.tight_layout()
             plt.pie(answers_counts.values(), labels=answers_counts.keys(), autopct=percentage_format, shadow=True)
-        plt.title(question)
-        plt.show()
+        plt.savefig(f"output/question{i+1}.png")
+        plt.close()
 
 
 if __name__ == "__main__":
